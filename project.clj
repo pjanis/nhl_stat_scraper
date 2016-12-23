abeb088cf7308fb7d8f4c518ff2f9820357f7313
Philip Janisiewicz <pjanisiewicz@gmail.com>
2016-12-22 20:14:37 -0500

    moved testing version to production

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nhl_stat_scraper "0.3.7-SNAPSHOT"
+(defproject nhl_stat_scraper "0.3.8-SNAPSHOT"
   :description "App to create database with NHL game data"
   :url ""
   :license {:name "Eclipse Public License"
